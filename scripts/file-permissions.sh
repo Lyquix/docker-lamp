9ada57ce1ca20e6b771beb76c2e2e38864f6997d
rubenreyes2000 <ruben.reyes@lyquix.com>
2023-12-21 11:17:14 -0500

    Improvements to file-permissions.sh
    * Add option to fix permissions for theme/template folder only
    * More verbose output
    * Do not update theme/template folder if it doesn't exist

--- scripts/file-permissions.sh
+++ scripts/file-permissions.sh
@@ -25,49 +25,137 @@ fi
 
 DIVIDER="\n***************************************\n\n"
 CURRDIR="${PWD}"
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # Welcome and instructions
 printf $DIVIDER
 printf "Lyquix file permissions script\n"
 printf $DIVIDER
 
-# Prompt to continue
+# Prompt whether to do all sites or a specific one
 while true; do
-	read -p "Fix file permissions for ALL sites [Y/N]? " FIXALL
+	read -p "Fix ALL sites? [Y/N] " FIXALL
 	case $FIXALL in
-	[Y]*) break ;;
-	[y]*) break ;;
-	[N]*) break ;;
-	[n]*) break ;;
+	[YyNn]*) break ;;
 	*) printf "Please answer Y or N\n" ;;
 	esac
 done
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
+# Prompt whether to do the whole site or just the theme/template folder
+while true; do
+	read -p "Fix ONLY the theme/template files? [Y/N] " FIXTHEME
+	case $FIXTHEME in
+	[YyNn]*) break ;;
+	*) printf "Please answer Y or N\n" ;;
+	esac
+done
 
-if [ "$FIXALL" = "Y" ] || [ "$FIXALL" = "y" ]; then
-	printf "Updating file permissions for all sites, please wait...\n"
-	chown -R www-data:www-data /srv/www/*
-	find /srv/www/*/public_html -type f -exec chmod 666 {} \;
-	find /srv/www/*/public_html -type d -exec chmod 777 {} \;
-	chmod +x /srv/www/*/public_html/wp-content/themes/*lyquix*/node_modules/.bin/*
-	chmod +x /srv/www/*/public_html/templates/*lyquix*/node_modules/.bin/*
-	find /srv/www/*/public_html/wp-content/themes/*lyquix*/ -name "*.sh" -type f -exec chmod +x {} \;
-	find /srv/www/*/public_html/templates/*lyquix*/ -name "*.sh" -type f -exec chmod +x {} \;
+if [[ $FIXALL =~ ^[Yy]$ ]]; then
+	WP="$(find /srv/www/*/public_html/wp-content/themes/*lyquix* -maxdepth 0 -type d 2>/dev/null | wc -l)"
+	JOOMLA="$(find /srv/www/*/public_html/templates/*lyquix* -maxdepth 0 -type d 2>/dev/null | wc -l)"
+
+	if [[ $FIXTHEME =~ ^[Nn]$ ]]; then
+		printf "Updating ALL sites\n"
+		printf "Set www-data as owner\n"
+		chown -R www-data:www-data /srv/www/*
+		printf "Set permissions of files to 666\n"
+		find /srv/www/*/public_html -type f -exec chmod 666 {} \;
+		printf "Set permissions of directories to 777\n"
+		find /srv/www/*/public_html -type d -exec chmod 777 {} \;
+	else
+		printf "Updating ALL sites, ONLY theme/template files\n"
+		printf "Set www-data as owner\n"
+		if [ "$WP" != "0" ]; then
+			chown -R www-data:www-data /srv/www/*/public_html/wp-content/themes/*lyquix*
+		fi
+		if [ "$JOOMLA" != "0" ]; then
+			chown -R www-data:www-data /srv/www/*/public_html/templates/*lyquix*
+		fi
+		printf "Set permissions of files to 666\n"
+		if [ "$WP" != "0" ]; then
+			find /srv/www/*/public_html/wp-content/themes/*lyquix* -type f -exec chmod 666 {} \;
+		fi
+		if [ "$JOOMLA" != "0" ]; then
+			find /srv/www/*/public_html/templates/*lyquix* -type f -exec chmod 666 {} \;
+		fi
+		printf "Set permissions of directories to 777\n"
+		if [ "$WP" != "0" ]; then
+			find /srv/www/*/public_html/wp-content/themes/*lyquix* -type d -exec chmod 777 {} \;
+		fi
+		if [ "$JOOMLA" != "0" ]; then
+			find /srv/www/*/public_html/templates/*lyquix* -type d -exec chmod 777 {} \;
+		fi
+	fi
+	printf "Set execution permissions to node_modules/.bin\n"
+	if [ "$WP" != "0" ]; then
+		chmod +x /srv/www/*/public_html/wp-content/themes/*lyquix*/node_modules/.bin/*
+	fi
+	if [ "$JOOMLA" != "0" ]; then
+		chmod +x /srv/www/*/public_html/templates/*lyquix*/node_modules/.bin/*
+	fi
+	printf "Set execution permissions to shell scripts\n"
+	if [ "$WP" != "0" ]; then
+		find /srv/www/*/public_html/wp-content/themes/*lyquix*/ -name "*.sh" -type f -exec chmod +x {} \;
+	fi
+	if [ "$JOOMLA" != "0" ]; then
+		find /srv/www/*/public_html/templates/*lyquix*/ -name "*.sh" -type f -exec chmod +x {} \;
+	fi
 else
 	printf "Please select folder:\n"
 	select DIR in */; do
 		test -n "$DIR" && break
 		echo ">>> Invalid Selection"
 	done
-	printf "Updating file permissions for /srv/www/$DIR...\n"
-	chown -R www-data:www-data /srv/www/$DIR
-	find /srv/www/$DIR/public_html -type f -exec chmod 666 {} \;
-	find /srv/www/$DIR/public_html -type d -exec chmod 777 {} \;
-	chmod +x /srv/www/$DIR/public_html/wp-content/themes/*lyquix*/node_modules/.bin/*
-	chmod +x /srv/www/$DIR/public_html/templates/*lyquix*/node_modules/.bin/*
-	find /srv/www/$DIR/public_html/wp-content/themes/*lyquix*/ -name "*.sh" -type f -exec chmod +x {} \;
-	find /srv/www/$DIR/public_html/templates/*lyquix*/ -name "*.sh" -type f -exec chmod +x {} \;
+
+	WP="$(find /srv/www/$DIR/public_html/wp-content/themes/*lyquix* -maxdepth 0 -type d 2>/dev/null | wc -l)"
+	JOOMLA="$(find /srv/www/$DIR/public_html/templates/*lyquix* -maxdepth 0 -type d 2>/dev/null | wc -l)"
+
+	if [[ $FIXTHEME =~ ^[Nn]$ ]]; then
+		printf "Updating /srv/www/$DIR\n"
+		printf "Set www-data as owner\n"
+		chown -R www-data:www-data /srv/www/$DIR
+		printf "Set permissions of files to 666\n"
+		find /srv/www/$DIR/public_html -type f -exec chmod 666 {} \;
+		printf "Set permissions of directories to 777\n"
+		find /srv/www/$DIR/public_html -type d -exec chmod 777 {} \;
+	else
+		printf "Updating /srv/www/$DIR, ONLY theme/template files\n"
+		printf "Set www-data as owner\n"
+		if [ "$WP" != "0" ]; then
+			chown -R www-data:www-data /srv/www/$DIR/public_html/wp-content/themes/*lyquix*
+		fi
+		if [ "$JOOMLA" != "0" ]; then
+			chown -R www-data:www-data /srv/www/$DIR/public_html/templates/*lyquix*
+		fi
+		printf "Set permissions of files to 666\n"
+		if [ "$WP" != "0" ]; then
+			find /srv/www/$DIR/public_html/wp-content/themes/*lyquix* -type f -exec chmod 666 {} \;
+		fi
+		if [ "$JOOMLA" != "0" ]; then
+			find /srv/www/$DIR/public_html/templates/*lyquix* -type f -exec chmod 666 {} \;
+		fi
+		printf "Set permissions of directories to 777\n"
+		if [ "$WP" != "0" ]; then
+			find /srv/www/$DIR/public_html/wp-content/themes/*lyquix* -type d -exec chmod 777 {} \;
+		fi
+		if [ "$JOOMLA" != "0" ]; then
+			find /srv/www/$DIR/public_html/templates/*lyquix* -type d -exec chmod 777 {} \;
+		fi
+	fi
+	printf "Set execution permissions to node_modules/.bin\n"
+	if [ "$WP" != "0" ]; then
+		chmod +x /srv/www/$DIR/public_html/wp-content/themes/*lyquix*/node_modules/.bin/*
+	fi
+	if [ "$JOOMLA" != "0" ]; then
+		chmod +x /srv/www/$DIR/public_html/templates/*lyquix*/node_modules/.bin/*
+	fi
+	printf "Set execution permissions to shell scripts\n"
+	if [ "$WP" != "0" ]; then
+		find /srv/www/$DIR/public_html/wp-content/themes/*lyquix*/ -name "*.sh" -type f -exec chmod +x {} \;
+	fi
+	if [ "$JOOMLA" != "0" ]; then
+		find /srv/www/$DIR/public_html/templates/*lyquix*/ -name "*.sh" -type f -exec chmod +x {} \;
+	fi
 fi
 
 cd $CURRDIR
